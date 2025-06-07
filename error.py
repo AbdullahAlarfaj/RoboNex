c43967a4ef87d5587358b7e76b6774e11090bcc8
AbdullahAlarfaj <161985283+AbdullahAlarfaj@users.noreply.github.com>
2025-06-07 10:23:51 +0300

    Create error.py

---
+++ error.py
@@ -0,0 +1,44 @@
+pygame 2.6.1 (SDL 2.28.4, Python 3.11.2)
+Hello from the pygame community. https://www.pygame.org/contribute.html
+nameneutral
+Exception in thread Thread-1 (<lambda>):
+Traceback (most recent call last):
+  File "/usr/lib/python3.11/threading.py", line 1038, in _bootstrap_inner
+    self.run()
+  File "/usr/lib/python3.11/threading.py", line 975, in run
+    self._target(*self._args, **self._kwargs)
+  File "/home/admin/RoboNex/test.py", line 40, in <lambda>
+    threading.Thread(target=lambda: expression_loop(self, self.robot, self.controller), daemon=True).start()
+                                    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+TypeError: expression_loop() missing 1 required positional argument: 'stop_event'
+تشغيل الاستماع
+إيقاف مؤقت
+ استمع... (اضغط Space لإيقاف)
+namelistening
+تشغيل الاستماع
+إيقاف مؤقت
+ استمع... (اضغط Space لإيقاف)
+namelistening
+تشغيل الاستماع
+تشغيل الاستماع
+ استمع... (اضغط Space لإيقاف)
+namelistening
+استمع... (اضغط Space لإيقاف)
+namelistening
+robot
+robot
+robot
+robot
+robot
+robot
+robot
+pos
+robot
+robot
+robot
+robot
+robot
+pos
+robot
+robot
+robot
